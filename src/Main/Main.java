dc9b553758eca7ae6ff5a73f7a0c74d84ab6ca67
Connordore <cjcolbert159@gmail.com>
2021-04-19 10:20:59 -0400

    Added framerate limiter

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,13 +15,15 @@ public class Main {
     private static GameStateManager gameStateManager;
     private static Graphics g;
     
-    private static long time = System.nanoTime();
+    private static long time;
+    
+    private static final int TARGET_FRAME_RATE = 60;
     
     public static void initialize() {
-        
+    	time = System.nanoTime();
+    	
         gameStateManager = GameStateManager.Instance();
         gameStateManager.addScreen(new TitleScreen());
-        
     }
     
     public static void main(String[] args) {
@@ -33,7 +35,16 @@ public class Main {
             time = currentTime;
             gameStateManager.update((float)deltaTimeInSeconds);
             gameStateManager.draw();
-            // run gamecode here
+            
+            // Each frame should run for 1/TARGET_FRAME_RATE seconds, so I need to sleep each frame for that amount minus the time each frame takes 
+            // (though technically the deltaTime accounts for the previous frame but it still works)
+            long msToSleep = 1000 / (TARGET_FRAME_RATE - (long)deltaTimeInSeconds);
+            
+            try {
+            	Thread.sleep(msToSleep);
+            } catch (Exception e) {
+            	System.out.println("Couldn't sleep the update thread!");
+            }
         }
         System.exit(0);
     }
